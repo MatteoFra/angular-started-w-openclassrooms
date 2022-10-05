7d12b6a2d5b3c9bc81b55f84d10f8f6b9e1bbb54
MatteoFra <matteofra@outlook.fr>
2022-10-05 11:09:02 +0200

    train red/yellow

---
+++ simul-observable-int-ext.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { interval, of } from 'rxjs';
+import { concatMap, mergeMap, delay, exhaustMap, map, switchMap, take, tap } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+
+  redTrainsCalled = 0;
+  yellowTrainsCalled = 0;
+
+  ngOnInit() {
+    interval(500).pipe(
+      take(10),
+      map(value => value % 2 === 0 ? 'rouge' : 'jaune'),
+      tap(color => console.log(`La lumière s'allume en %c${color}`, `color: ${this.translateColor(color)}`)),
+      mergeMap(color => this.getTrainObservable$(color)),
+      tap(train => console.log(`Train %c${train.color} ${train.trainIndex} arrivé !`, `font-weight: bold; color: ${this.translateColor(train.color)}`))
+    ).subscribe();
+  }
+
+  getTrainObservable$(color: 'rouge' | 'jaune') {
+    const isRedTrain = color === 'rouge';
+    isRedTrain ? this.redTrainsCalled++ : this.yellowTrainsCalled++;
+    const trainIndex = isRedTrain ? this.redTrainsCalled : this.yellowTrainsCalled;
+    console.log(`Train %c${color} ${trainIndex} appelé !`, `text-decoration: underline; color: ${this.translateColor(color)}`);
+    return of({ color, trainIndex }).pipe(
+      delay(isRedTrain ? 5000 : 6000)
+    );
+  }
+
+  translateColor(color: 'rouge' | 'jaune') {
+    return color === 'rouge' ? 'red' : 'yellow';
+  }
+}
+
+// mergeMap
+// assure la mise en parallèle : l'Observable extérieur peut souscrire aux Observables
+// intérieurs suivants sans attendre que les précédents soient complétés.
+
+// concatMap
+// assure la mise en série : il attend que les Observables intérieurs complètent avant 
+// de souscrire aux suivants– même si l'Observable extérieur émet plusieurs fois. 
+// Les Observables intérieurs seront traités en séquence à la suite.
+
+// exhaustMap
+// assure le traitement complet d'une souscription avant d'observer une nouvelle émission 
+// de l'Observable extérieur. Si d’autres demandes sont faites entre temps, elles ne seront pas prises en compte. 
+
+// switchMap
+// traite la dernière demande de souscription de l’Observable extérieur et annule toute souscription précédente non-complétée.
