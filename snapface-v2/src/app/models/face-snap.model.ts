36a0197730ad807ee9ba1e4acfd18df2a7b1c3f7
MatteoFra <matteofra@outlook.fr>
2022-10-05 11:06:48 +0200

    start course

---
+++ snapface-v2/src/app/models/face-snap.model.ts
@@ -0,0 +1,40 @@
+import { Input } from '@angular/core';
+
+export class FaceSnap {
+  id!: number;
+  title!: string;
+  description!: string;
+  imageUrl!: string;
+  createdDate!: Date;
+  snaps!: number;
+  location?: string;
+
+  // constructor(public title: string,
+  //             public description: string,
+  //             public imageUrl: string,
+  //             public createdDate: Date,
+  //             public snaps: number) {
+  // }
+}
+
+// export class FaceSnap {
+//   title: string;
+//   description: string;
+//   createdDate: Date;
+//   snaps: number;
+//   imageUrl: string;
+
+//   constructor(
+//     title: string,
+//     description: string,
+//     imageUrl: string,
+//     createdDate: Date,
+//     snaps: number
+//   ) {
+//     this.title = title;
+//     this.description = description;
+//     this.imageUrl = imageUrl;
+//     this.createdDate = createdDate;
+//     this.snaps = snaps;
+//   }
+// }
