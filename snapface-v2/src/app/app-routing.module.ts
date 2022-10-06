5e671b13f7733c04f1c53a0b9057d5d6af9a9de5
MatteoFra <matteofra@outlook.fr>
2022-10-06 12:02:18 +0200

    refactoring w coreModule & featureModule

--- snapface-v2/src/app/app-routing.module.ts
+++ snapface-v2/src/app/app-routing.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FaceSnapListComponent } from './components/face-snap-list/face-snap-list.component';
-import { HeaderComponent } from './components/header/header.component';
-import { LandingPageComponent } from './components/landing-page/landing-page.component';
-import { NewFaceSnapComponent } from './components/new-face-snap/new-face-snap.component';
-import { SingleFaceSnapComponent } from './components/single-face-snap/single-face-snap.component';
+import { LandingPageComponent } from './landing-page/components/landing-page/landing-page.component';
 
 const routes: Routes = [
+  {
+    path: 'facesnaps',
+    loadChildren: () =>
+      import('./face-snaps/face-snaps.module').then((m) => m.FaceSnapsModule),
+  },
   { path: '', component: LandingPageComponent },
-  { path: 'facesnaps', component: FaceSnapListComponent },
-  { path: 'facesnaps/:id', component: SingleFaceSnapComponent },
-  { path: 'create', component: NewFaceSnapComponent }
 ];
 
 @NgModule({
