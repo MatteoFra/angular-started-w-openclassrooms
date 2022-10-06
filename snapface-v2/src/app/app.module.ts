63ca85618bb770be65d256de0e3faec9dfd69583
MatteoFra <matteofra@outlook.fr>
2022-10-06 12:02:18 +0200

    refactoring w coreModule & featureModule

--- snapface-v2/src/app/app.module.ts
+++ snapface-v2/src/app/app.module.ts
@@ -1,41 +1,29 @@
-import { LOCALE_ID, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { FaceSnapComponent } from './components/face-snap/face-snap.component';
+import { FaceSnapComponent } from './face-snaps/components/face-snap/face-snap.component';
 import { registerLocaleData } from '@angular/common';
 import * as fr from '@angular/common/locales/fr';
-import { FaceSnapListComponent } from './components/face-snap-list/face-snap-list.component';
-import { HeaderComponent } from './components/header/header.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
-import { LandingPageComponent } from './components/landing-page/landing-page.component';
-import { SingleFaceSnapComponent } from './components/single-face-snap/single-face-snap.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NewFaceSnapComponent } from './components/new-face-snap/new-face-snap.component';
-import { httpInterceptorProviders } from './interceptors';
+import { CoreModule } from './core/core.module';
+import { FaceSnapsModule } from './face-snaps/face-snaps.module';
+import { LandingPageModule } from './landing-page/landing-page.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FaceSnapComponent,
-    FaceSnapListComponent,
-    HeaderComponent,
-    LandingPageComponent,
-    SingleFaceSnapComponent,
-    NewFaceSnapComponent,
   ],
   imports: [
     BrowserModule, 
     AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule
+    CoreModule,
+    LandingPageModule,
+    HttpClientModule,
   ],
-  providers: [
-    { provide: LOCALE_ID, useValue: 'fr-FR' },
-    httpInterceptorProviders
-  ],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {
